ddc43b6ee4aac8f9b5c9904cb6ddcd84f18d4cff
lofilobzik <lobziklord@gmail.com>
2021-10-12 19:42:33 +0300

    Camera model in progress

--- camera-model/solidpython.py
+++ camera-model/solidpython.py
@@ -1,8 +1,17 @@
 from solid import *
 from solid.utils import *
 
-camera = cube([96, 4, 27]) + translate([(96-57)/2, 4, (27-21)/2])(cube([57, 16, 21])) + rotate()(cylinder(4, 9))
+SEGMENTS = 50
+f = 4
 
-result = camera
+for i in []: front_camera = rotate([90, 0, 0])(cylinder(5, 4))
 
-scad_render_to_file(result, 'result.scad')
+camera = cube([96, 4, 27]) + translate([(96-57)/2, 4, (27-21)/2])(cube([57, 16, 21]))
+for i in [27/2, 96-27/2]: camera += translate([i, 9+4, 27/2])(rotate([90, 0, 0])(cylinder(4, 9)))
+
+bottom = cube([200, 200, f])
+
+# result = translate([100-96/2, 0, f])(camera) + bottom
+result = front_camera
+
+scad_render_to_file(result, 'result.scad', file_header=f'$fn = {SEGMENTS};')
