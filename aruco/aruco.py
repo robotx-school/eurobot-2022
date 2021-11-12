9bb8468a9d338e9b4889c7f17253955b3fb52303
lofilobzik <lobziklord@gmail.com>
2021-11-12 14:17:26 +0300

    aruco.py code refactoring and camera-model changes

--- aruco/aruco.py
+++ aruco/aruco.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import numpy as np
 import cv2
 import matplotlib.path as mplPath
@@ -5,26 +7,6 @@ import struct
 import serial
 from datetime import datetime
 import pathlib
-
-logging_dir = '/home/pi/net/aruco/logs/'
-pathlib.Path(logging_dir).mkdir(parents=True, exist_ok=True)
-
-filename = logging_dir + str(datetime.now().strftime("%Y-%m-%d-%H-%M-%S")) + '.log'
-open(filename, 'w').close()
-
-botmarkers = [[130, 131, 132, 133], [134, 135, 136, 137], [139, 140, 141, 142], [143, 144, 145, 146]]
-byte_data = 0
-
-port = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-port.flush()
-
-dims = (854, 480)
-
-cap = cv2.VideoCapture(0)
-cap.set(3, dims[0])
-cap.set(4, dims[1])
-
-dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 
 def log_coord(coord, filename):
     with open(filename, 'a') as file:
@@ -55,116 +37,7 @@ def find_point(x1_1,y1_1,x1_2,y1_2,x2_1,y2_1,x2_2,y2_2):
     else:
         return None
 
-def get_coord_img(image_robots, image_field):
-    koef_H=[0.11,0.22,0.34,0.5,0.9]
-    koef_W=[(0.15,-0.3),(0.23,-0.15),(0.33,0.13),(0.42,0.34),(0.5,0.5),(0.58,0.66),(0.67,0.87),(0.77,1.15),(0.85,1.3)]
-    gray = cv2.cvtColor(image_robots,cv2.COLOR_BGR2GRAY)
-    image_robots = cv2.cvtColor(gray,cv2.COLOR_GRAY2RGB)
-    h=image_robots.shape[0]
-    w=image_robots.shape[1]
-    cross_points=[]
-    tiles=[]
-    tiles_coord=[]
-    for i in range(-1,len(koef_H)+1):
-        row=[]
-        if i==-1:
-            x1=0
-            y1=0
-            x2=w
-            y2=0
-        elif i==len(koef_H):
-            x1=0
-            y1=h
-            x2=w
-            y2=h
-        else:
-            x1=0
-            y1=int(h*koef_H[i])
-            x2=w
-            y2=int(h*koef_H[i])
-        cv2.line(image_robots,(x1,y1),(x2,y2),(128,128,0),5)
-        for j in range(-1,len(koef_W)+1):
-            if j==-1:
-                x3=0
-                y3=0
-                x4=0
-                y4=h
-            elif j==len(koef_W):
-                x3=w
-                y3=0
-                x4=w
-                y4=h
-            else:
-                x3=int(w*koef_W[j][0])
-                y3=0
-                x4=int(w*koef_W[j][1])
-                y4=h
-            cv2.line(image_robots,(x3,y3),(x4,y4),(128,0,128),5)
-            point=find_point(x1-2000,y1,x2+2000,y2,x3,y3,x4,y4)
-            row.append(point)
-            #print(i,j,point,x1,y1,x2,y2,x3,y3,x4,y4)
-        #print()
-        cross_points.append(row)
-    for i in range(6):
-        row=[]
-        row_c=[]
-        for j in range(10):
-            tile_c = ((cross_points[i][j]),
-                    (cross_points[i][j+1]),
-                    (cross_points[i+1][j+1]),
-                    (cross_points[i+1][j]))
-            tile = mplPath.Path(np.array([[cross_points[i][j][0], cross_points[i][j][1]],
-                                       [cross_points[i][j+1][0], cross_points[i][j+1][1]],
-                                       [cross_points[i+1][j+1][0], cross_points[i+1][j+1][1]],
-                                       [cross_points[i+1][j][0],cross_points[i+1][j][1]]
-                                    ]))
-            row.append(tile)
-            row_c.append(tile_c)
-        tiles.append(row)
-        tiles_coord.append(row_c)
-
-    markers = cv2.aruco.detectMarkers(img,dictionary)
-    count=0
-    for i in range(0,w,w//10):
-        for j in range(0,h,h//7):
-            t=3
-            c=(255,128,0)
-            c1=(i,j)
-            c2=(i+w//10,j+h//7)
-            cv2.rectangle(image_field, c1, c2, c, t)
-    for marker in markers[0]:
-        x1=int(marker[0][0][0])
-        y1=int(marker[0][0][1])
-        x2=int(marker[0][1][0])
-        y2=int(marker[0][1][1])
-        x3=int(marker[0][2][0])
-        y3=int(marker[0][2][1])
-        x4=int(marker[0][3][0])
-        y4=int(marker[0][3][1])
-        robot_pos=find_point(x1,y1,x3,y3,x2,y2,x4,y4)
-        marker_id = markers[1][count][0]
-        cv2.circle(image_robots,robot_pos,15,(0,0,120),-1)
-        for i in range(6):
-            for j in range(10):
-                if tiles[i][j].contains_point(robot_pos):
-                    print(i,j)
-                    pos=(j,i)
-                    cv2.line(image_robots,tiles_coord[i][j][-1],tiles_coord[i][j][0],(128,0,0),8)
-                    cv2.line(image_robots,tiles_coord[i][j][0],tiles_coord[i][j][1],(128,0,0),8)
-                    cv2.line(image_robots,tiles_coord[i][j][1],tiles_coord[i][j][2],(128,0,0),8)
-                    cv2.line(image_robots,tiles_coord[i][j][2],tiles_coord[i][j][3],(128,0,0),8)
-                    cv2.rectangle(image_field, ((j*w//10),(i*h//7)), ((j*w//10)+w//10,(i*h//7)+h//7), (255,0,255), 5)
-        count+=1
-    scale= 30
-    width = int(image_robots.shape[1] * scale / 100)
-    height = int(image_robots.shape[0] * scale / 100)
-    dim = (width, height)
-    image_robots = cv2.resize(image_robots, dim, interpolation = cv2.INTER_AREA)
-    image_field = cv2.resize(image_field, dim, interpolation = cv2.INTER_AREA)
-    image_res = np.vstack([image_robots,image_field])
-    return image_res
-
-def get_coord(image_robots, dictionary, botmarkers):
+def get_coord(image_robots, dictionary, botmarkers, tiles):
     bots = [[255, 255], [255, 255], [255, 255], [255, 255]]
     markers = cv2.aruco.detectMarkers(image_robots,dictionary)
     count=0
@@ -193,109 +66,152 @@ def get_coord(image_robots, dictionary, botmarkers):
 
     return bots
 
-koef_H=[0.11,0.22,0.34,0.5,0.9]
-koef_W=[(0.15,-0.3),(0.23,-0.15),(0.33,0.13),(0.42,0.34),(0.5,0.5),(0.58,0.66),(0.67,0.87),(0.77,1.15),(0.85,1.3)]
-cross_points=[]
-tiles=[]
-tiles_coord=[]
-w=dims[0]
-h=dims[1]
+def main():
+    logging_dir = '/home/pi/net/aruco/logs/'
+    pathlib.Path(logging_dir).mkdir(parents=True, exist_ok=True)
 
-for i in range(-1,len(koef_H)+1):
-    row=[]
-    if i==-1:
-        x1=0
-        y1=0
-        x2=w
-        y2=0
-    elif i==len(koef_H):
-        x1=0
-        y1=h
-        x2=w
-        y2=h
-    else:
-        x1=0
-        y1=int(h*koef_H[i])
-        x2=w
-        y2=int(h*koef_H[i])
-    for j in range(-1,len(koef_W)+1):
-        if j==-1:
-            x3=0
-            y3=0
-            x4=0
-            y4=h
-        elif j==len(koef_W):
-            x3=w
-            y3=0
-            x4=w
-            y4=h
+    filename = logging_dir + str(datetime.now().strftime("%Y-%m-%d-%H-%M-%S")) + '.log'
+    open(filename, 'w').close()
+
+    botmarkers = [[130, 131, 132, 133], [134, 135, 136, 137], [139, 140, 141, 142], [143, 144, 145, 146]]
+    byte_data = 0
+
+    port = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+    port.flush()
+
+    dims = (854, 480)
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3, dims[0])
+    cap.set(4, dims[1])
+
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+
+    koef_H = [0.11,0.22,0.34,0.5,0.9]
+    koef_W = [(0.15,-0.3),(0.23,-0.15),(0.33,0.13),(0.42,0.34),(0.5,0.5),(0.58,0.66),(0.67,0.87),(0.77,1.15),(0.85,1.3)]
+    cross_points = []
+    tiles=[]
+    tiles_coord=[]
+    w=dims[0]
+    h=dims[1]
+
+    for i in range(-1,len(koef_H)+1):
+        row=[]
+        if i==-1:
+            # x1=0
+            # y1=0
+            # x2=w
+            # y2=0
+
+            x1, y1, x2, y2 = 0, 0, w, 0
+
+        elif i==len(koef_H):
+            # x1=0
+            # y1=h
+            # x2=w
+            # y2=h
+
+            x1, y1, x2, y2 = 0, h, w, h
+
         else:
-            x3=int(w*koef_W[j][0])
-            y3=0
-            x4=int(w*koef_W[j][1])
-            y4=h
-        point=find_point(x1-2000,y1,x2+2000,y2,x3,y3,x4,y4)
-        row.append(point)
-    cross_points.append(row)
-for i in range(6):
-    row=[]
-    row_c=[]
-    for j in range(10):
-        tile_c = ((cross_points[i][j]),
-                (cross_points[i][j+1]),
-                (cross_points[i+1][j+1]),
-                (cross_points[i+1][j]))
-        tile = mplPath.Path(np.array([[cross_points[i][j][0], cross_points[i][j][1]],
-                                   [cross_points[i][j+1][0], cross_points[i][j+1][1]],
-                                   [cross_points[i+1][j+1][0], cross_points[i+1][j+1][1]],
-                                   [cross_points[i+1][j][0],cross_points[i+1][j][1]]
-                                ]))
-        row.append(tile)
-        row_c.append(tile_c)
-    tiles.append(row)
-    tiles_coord.append(row_c)
+            # x1=0
+            # y1=int(h*koef_H[i])
+            # x2=w
+            # y2=int(h*koef_H[i])
 
-old_data = [[255, 255], [255, 255], [255, 255], [255, 255]]
+            x1, y1, x2, y2 = 0, int(h*koef_H[i]), w, int(h*koef_H[i])
 
-while True:
-    _, img = cap.read()
+        for j in range(-1,len(koef_W)+1):
+            if j==-1:
+                # x3=0
+                # y3=0
+                # x4=0
+                # y4=h
 
-    data = get_coord(img, dictionary, botmarkers)
+                x3, y3, x4, y4 = 0, 0, 0, h
 
-    old_c = [item for sublist in old_data for item in sublist].count(255)
-    data_c = [item for sublist in data for item in sublist].count(255)
+            elif j==len(koef_W):
+                # x3=w
+                # y3=0
+                # x4=w
+                # y4=h
+                
+                x3, y3, x4, y4 = w, 0, w, h
 
-    data = data if data_c <= old_c else old_data
+            else:
+                # x3=int(w*koef_W[j][0])
+                # y3=0
+                # x4=int(w*koef_W[j][1])
+                # y4=h
 
-    if old_data != data:
-        if not pathlib.Path(filename).is_file():
-            open(filename, 'w').close()
+                x3, y3, x4, y4 = int(w*koef_W[j][0]), 0, int(w*koef_W[j][1]), h
 
-        log_coord(data, filename)
-        old_data = data
-        print(data)
+            point=find_point(x1-2000,y1,x2+2000,y2,x3,y3,x4,y4)
+            row.append(point)
+        cross_points.append(row)
+    for i in range(6):
+        row=[]
+        row_c=[]
 
-    raw_data = sum(data, [])
+        for j in range(10):
+            tile_c = ((cross_points[i][j]),
+                    (cross_points[i][j+1]),
+                    (cross_points[i+1][j+1]),
+                    (cross_points[i+1][j]))
+            tile = mplPath.Path(np.array([[cross_points[i][j][0], cross_points[i][j][1]],
+                                    [cross_points[i][j+1][0], cross_points[i][j+1][1]],
+                                    [cross_points[i+1][j+1][0], cross_points[i+1][j+1][1]],
+                                    [cross_points[i+1][j][0],cross_points[i+1][j][1]]
+                                    ]))
+            row.append(tile)
+            row_c.append(tile_c)
+        tiles.append(row)
+        tiles_coord.append(row_c)
 
-    for i in range(len(raw_data)):
-        if isinstance(raw_data[i], tuple):
-            raw_data[i] = raw_data[i][0]<<4 | raw_data[i][1]
+    old_data = [[255, 255], [255, 255], [255, 255], [255, 255]]
 
-    byte_data = struct.pack('cccccccc',
-                raw_data[0].to_bytes(1, 'big'),
-                raw_data[1].to_bytes(1, 'big'),
-                raw_data[2].to_bytes(1, 'big'),
-                raw_data[3].to_bytes(1, 'big'),
-                raw_data[4].to_bytes(1, 'big'),
-                raw_data[5].to_bytes(1, 'big'),
-                raw_data[6].to_bytes(1, 'big'),
-                raw_data[7].to_bytes(1, 'big'),)
+    while True:
+        _, img = cap.read()
 
-    port.write(byte_data)
+        data = get_coord(img, dictionary, botmarkers, tiles)
 
-    data = old_data
+        old_c = [item for sublist in old_data for item in sublist].count(255)
+        data_c = [item for sublist in data for item in sublist].count(255)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-cap.release()
-cv2.destroyAllWindows()
+        data = data if data_c <= old_c else old_data
+
+        if old_data != data:
+            if not pathlib.Path(filename).is_file():
+                open(filename, 'w').close()
+
+            log_coord(data, filename)
+            old_data = data
+            print(data)
+
+        raw_data = sum(data, [])
+
+        for i in range(len(raw_data)):
+            if isinstance(raw_data[i], tuple):
+                raw_data[i] = raw_data[i][0]<<4 | raw_data[i][1]
+
+        byte_data = struct.pack('cccccccc',
+                    raw_data[0].to_bytes(1, 'big'),
+                    raw_data[1].to_bytes(1, 'big'),
+                    raw_data[2].to_bytes(1, 'big'),
+                    raw_data[3].to_bytes(1, 'big'),
+                    raw_data[4].to_bytes(1, 'big'),
+                    raw_data[5].to_bytes(1, 'big'),
+                    raw_data[6].to_bytes(1, 'big'),
+                    raw_data[7].to_bytes(1, 'big'),)
+
+        port.write(byte_data)
+
+        data = old_data
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
--- camera-model/solidpython.py
+++ camera-model/solidpython.py
@@ -1,75 +1,310 @@
+#!/usr/bin/python3
+
 from solid import *
 from solid.utils import *
+import math as m
 
 def main():
-  SEGMENTS = 200
-  f = 4
+    SEGMENTS = 200
+    f = 4
   
-  #-- camera
-  front_camera = 0
-  for i in [[0, 0, 0], [86, 0, 0], [0, 0, 17], [86, 0, 17]]: front_camera += translate(i)(rotate([90, 0, 0])(cylinder(5, 4)))
-  front_camera = hull()(front_camera)
-  front_camera_extrude = 0
-  for i in [[0, 0, 0], [86, 0, 0], [0, 17, 0], [86, 17, 0]]: front_camera_extrude += translate([5, 5, -1])(translate(i)(cylinder(5, f+2)))
-  front_camera_extrude = hull()(front_camera_extrude)
-  camera = translate([5, f, 5])(front_camera) + translate([(96-57)/2, 4, (27-21)/2])(cube([57, 16, 21]))
-  for i in [27/2, 96-27/2]: camera += translate([i, 9+4, 27/2])(rotate([90, 0, 0])(cylinder(4, 9)))
-  camera = color('grey')(camera)
-  
-  #-- bottom
-  hole = cube([f+1, 20, f+2])
-  hole_w_1 = cube([f+1, 21, f+2])
-  big_hole = cube([40, f+1, f+2])
-  bottom = cube([200, 200, f]) - translate([-1, -1, -1])(hole_w_1) - translate([200-f, -1, -1])(hole_w_1)
-  for i in range(40, 200, 40): bottom -= translate([-1, i, -1])(hole) + translate([200-f, i, -1])(hole)
-  for i in [40, 120]: bottom -= translate([i, 200-f, -1])(big_hole)
-  bottom -= translate([100, 100, -1])(cylinder(h=f+2, d=10))
-  
-  #-- front part
-  bolt_ext = translate([0, -1, -1])(cube([3, 13+1, f+2])) + translate([-(5.5-3)/2, 13/2-2.5/2, -1])(cube([5.5, 2.5, f+2]))
-  fr_part_h = 50-f
-  front_part = cube([200-f*2, fr_part_h, f]) - translate([(200-f*2-96)/2, (fr_part_h-27)/2, 0])(front_camera_extrude)
-  h = 42
-  front_part -= forward(h)(rotate([0, 0, -90])(bolt_ext))
-  front_part -= translate([200-f*2, h-3, 0])(rotate([0, 0, 90])(bolt_ext))
-  h = 0
-  
-  #-- side
-  lean_offset = 3
-  side = translate([0, 50-fr_part_h, 0])(linear_extrude(f)(arc(rad=fr_part_h, start_degrees=90, end_degrees=135+lean_offset))) + cube([200, 50, f]) - rotate(-90)(translate([-f, 180, -1])(hole_w_1))
-  side -= translate([0, f, -1])(linear_extrude(f+2)(arc(rad=fr_part_h-f, start_degrees=90+lean_offset, end_degrees=135+lean_offset+1)))
-  side += translate([0, f, 0])(linear_extrude(f)(arc(rad=fr_part_h-f-3, start_degrees=90+lean_offset, end_degrees=135+lean_offset)))
-  side += forward(f)()(rotate([0, 0, 45+lean_offset])(left(f)(cube([4, fr_part_h, f])) + linear_extrude(f)(arc(rad=f, start_degrees=180, end_degrees=225))))
-  for i in [lean_offset, 45+lean_offset]: side -= translate([0, f, -1])(rotate([0, 0, i])(forward(fr_part_h-1.5-f)(cylinder(h=f+2, d=3))))
-  for i in range(20, 180, 40): side -= rotate(-90)(translate([-f, i, -1])(hole))
-  side -= translate([200-f, 15+f/2, -1])(cube([f+1, 20, f+2]))
-  
-  #-- back_part
-  back_part = translate([f, f, 0])(cube([200-f*2, 50-f, f]))
-  for i in [0, 200-f]: back_part += translate([i, 15+f/2, 0])(cube([f, 20, f]))
-  for i in [40, 120]: back_part += translate([i, 0, 0])(cube([40, f, f]))
-  
-  #-- front part with camera
-  deg = 45
-  front_part_w_camera = up(f)(rotate([90+deg, 0, 0])(right(f)(front_part))) + up(f)(rotate([deg])(translate([(200-96)/2, -f, (fr_part_h-27)/2])(camera)))
-  deg = 0
-  
-  #-- model
-  model = bottom
-  model += rotate([90, 0, 90])(side) + right(200-f)(rotate([90, 0, 90])(side))
-  model += front_part_w_camera
-  model += translate([0, 200, 0])(rotate([90, 0, 0])(back_part))
-  
-  scad_render_to_file(model, 'model.scad', file_header=f'$fn = {SEGMENTS};')
-  
-  #-- result
-  result = bottom
-  for i in range(1, 3): result += translate([200+52*i, 35.3, 0])(rotate([0, 0, 90])(side))
-  result += translate([0, 202, 0])(front_part)
-  result += translate([0, 202+48, 0])(back_part)
-  result = projection()(result)
-  
-  scad_render_to_file(result, 'result.scad', file_header=f'$fn = {SEGMENTS};')
+    #-- camera
+
+    front_camera = 0
+    for i in [[0, 0, 0], [86, 0, 0], [0, 0, 17], [86, 0, 17]]:
+        front_camera += \
+        translate(i)(
+        rotate([90, 0, 0])(
+        cylinder(5, 4)
+        ))
+
+    front_camera = \
+    hull()(
+    front_camera
+    )
+
+    front_camera_extrude = 0
+    for i in [[0, 0, 0], [86, 0, 0], [0, 17, 0], [86, 17, 0]]:
+        front_camera_extrude += \
+        translate([5, 5, -1])(
+        translate(i)(
+        cylinder(5, f+2)
+        ))
+
+    front_camera_extrude = \
+    hull()(
+    front_camera_extrude
+    )
+
+    camera = \
+    translate([5, f, 5])(front_camera) + \
+    translate([(96-57)/2, 4, (27-21)/2])(
+    cube([57, 16, 21])
+    )
+
+    for i in [27/2, 96-27/2]:
+        camera += \
+        translate([i, 9+4, 27/2])(
+        rotate([90, 0, 0])(
+        cylinder(4, 9)
+        ))
+
+    camera = \
+    color('grey')(
+    camera
+    )
+
+    #-- bottom
+
+    hole = \
+    cube([f+1, 20, f+2])
+
+    hole_w_1 = \
+    cube([f+1, 21, f+2])
+
+    big_hole = \
+    cube([40, f+1, f+2])
+
+    bottom = \
+    cube([200, 200, f]) - \
+    translate([-1, -1, -1])(
+    hole_w_1
+    ) - \
+    translate([200-f, -1, -1])(
+    hole_w_1
+    )
+
+    for i in range(40, 200, 40):
+        bottom -= \
+        translate([-1, i, -1])(
+        hole
+        ) + \
+        translate([200-f, i, -1])(
+        hole
+        )
+    
+    for i in [40, 120]:
+        bottom -= \
+        translate([i, 200-f, -1])(
+        big_hole
+        )
+
+    bottom -= \
+    translate([100, 100, -1])(
+    cylinder(h=f+2, d=10)
+    )
+
+    #-- front part
+
+    bolt_ext = \
+    translate([0, -1, -1])(
+    cube([3, 13+1, f+2])
+    ) + \
+    translate([-(5.5-3)/2, 13/2-2.5/2, -1])(
+    cube([5.5, 2.5, f+2])
+    )
+
+    fr_part_h = 50-f
+
+    front_part = \
+    cube([200-f*2, fr_part_h, f]) - \
+    translate([(200-f*2-96)/2, (fr_part_h-27)/2, 0])(
+    front_camera_extrude
+    )
+
+    dip = 1.5 + 4
+    front_part += \
+    translate([0, -dip, 0])(
+    cube([200 - f * 2, dip, f])
+    )
+    dip = 0
+
+    h = 42
+    h_2 = 1.5
+    for i in (h, h_2):
+        front_part -= \
+        forward(i)(
+        rotate([0, 0, -90])(
+        bolt_ext
+        ))
+
+        front_part -= \
+        translate([200-f*2, i-3, 0])(
+        rotate([0, 0, 90])(
+        bolt_ext
+        ))
+    h = 0
+
+    #-- side
+
+    lean_offset = 0
+
+    side = \
+    cube([200, 50, f]) - \
+    rotate(-90)(
+    translate([-f, 180, -1])(
+    hole_w_1
+    ))
+
+    side += \
+    translate([-6, f, 0])(
+    linear_extrude(f)(
+    arc(rad=fr_part_h, start_degrees=90+lean_offset, end_degrees=135+lean_offset)
+    ))
+
+    side += \
+    translate([-6, f, 0])(
+    rotate([0, 0, 135])(
+    translate([-4, 0, 0])(
+    cube([fr_part_h + 4, f + 1.5, f])
+    )))
+
+    side += \
+    mirror([1, 0, 0])(
+    cube([6, fr_part_h + f, f])
+    )
+
+    side -= \
+    translate([0, 0, -0.5])(
+    rotate([0, 0, 180])(
+    cube([15, 5, f + 1])
+    ))
+
+    side -= \
+    translate([-6, f, -0.5])(
+    linear_extrude(f + 1)(
+    arc(rad=fr_part_h - f, start_degrees=90+lean_offset, end_degrees=135+lean_offset)
+    ))
+
+    side += \
+    translate([-6, f, 0])(
+    linear_extrude(f)(
+    arc(rad=fr_part_h - f - 3, start_degrees=90+lean_offset, end_degrees=135+lean_offset)
+    ))
+
+    for i in (0, 45):
+        side -= \
+        translate([-6, f, -0.5])(
+        rotate([0, 0, i])(
+        forward(fr_part_h-1.5-f)(
+        cylinder(h=f + 1, d=3)
+        )))
+
+    side -= \
+    translate([-6, f, -0.5])(
+    cylinder(h=f + 1, d=3)
+    )
+
+    side -= \
+    translate([-6, f, -0.5])(
+    rotate([0, 0, 45])(
+    forward(fr_part_h-1.5-f)(
+    translate([-6, 0, 0])(cube([6, 6, f + 1])) - \
+    cylinder(h=f + 1, r = 5.5)
+    )))
+
+    side -= \
+    translate([-7.5, 5.5, -0.5])(
+    rotate([0, 0, 0])(
+    translate([-3.9, -6, 0])(cube([3.9, 6, f + 1])) - \
+    cylinder(h=f + 1, r = 5.5)
+    ))
+
+    for i in range(20, 180, 40):
+        side -= \
+        rotate(-90)(
+        translate([-f, i, -1])(
+        hole
+        ))
+
+    side -= \
+    translate([200-f, 15+f/2, -1])(
+    cube([f+1, 20, f+2])
+    )
+
+    #-- back_part
+
+    back_part = \
+    translate([f, f, 0])(
+    cube([200-f*2, 50-f, f])
+    )
+
+    for i in [0, 200-f]:
+        back_part += \
+        translate([i, 15+f/2, 0])(
+        cube([f, 20, f])
+        )
+
+    for i in [40, 120]:
+        back_part += \
+        translate([i, 0, 0])(
+        cube([40, f, f])
+        )
+
+    #-- front part with camera
+
+    deg = 45
+    front_part_w_camera = \
+    translate([0, -6, f])(
+    rotate([90+deg, 0, 0])(
+    translate([f, 0, -f/2])(
+    front_part
+    )))
+    deg = 0
+
+    #-- model
+
+    model = \
+    bottom
+
+    model += \
+    rotate([90, 0, 90])(
+    side
+    ) + \
+    right(200-f)(
+    rotate([90, 0, 90])(
+    side
+    ))
+
+    model += \
+    front_part_w_camera
+
+    model += \
+    translate([0, 200, 0])(
+    rotate([90, 0, 0])(
+    back_part
+    ))
+
+    scad_render_to_file(model, 'model.scad', file_header=f'$fn = {SEGMENTS};')
+
+    #-- result
+
+    result = \
+    bottom
+
+    for i in range(1, 3):
+        result += \
+        translate([200+52*i, 40.2, 0])(
+        rotate([0, 0, 90])(
+        side
+        ))
+
+    result += \
+    translate([0, 208, 0])(
+    front_part
+    )
+
+    result += \
+    translate([0, 208+48, 0])(
+    back_part
+    )
+
+    result = \
+    projection()(
+    result
+    )
+
+    scad_render_to_file(result, 'result.scad', file_header=f'$fn = {SEGMENTS};')
   
 if __name__ == '__main__':
-  main()
+    main()
