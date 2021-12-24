06bf85bbbc6997bc19722e2c5971e604e8c0455a
ret7020 <55328925+ret7020@users.noreply.github.com>
2021-12-24 15:18:55 +0300

    Create prepare.sh

---
+++ doc/torch-on-rsp/build-on-rsp/prepare.sh
@@ -0,0 +1,5 @@
+sudo apt-get install ninja-build git cmake
+sudo apt-get install libopenmpi-dev libomp-dev ccache
+sudo apt-get install libopenblas-dev libblas-dev libeigen3-dev
+sudo -H pip3 install -U --user wheel mock pillow
+sudo -H pip3 install -U setuptools
