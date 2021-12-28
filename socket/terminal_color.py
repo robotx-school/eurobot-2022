4a410f2cb78b23e01be9e92c545538a566208734
ret7020 <55328925+ret7020@users.noreply.github.com>
2021-12-29 00:47:22 +0300

    Create terminal_color.py

---
+++ socket/terminal_color.py
@@ -0,0 +1,6 @@
+def out_red(text):
+    return "\033[31m {}" .format(text)
+def out_yellow(text):
+    return "\033[33m {}" .format(text)
+def out_green(text):
+    return "\033[32m {}" .format(text)
