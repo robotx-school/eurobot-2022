7f202fabf3d484e3cfa6655841581151ec9c579e
lofilobzik <75563134+lofilobzik@users.noreply.github.com>
2022-02-08 18:34:45 +0300

    Update setup.sh

--- jetson/setup.sh
+++ jetson/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade && sudo apt autoremove
-sudo apt install nano tmux htop
+sudo apt update && sudo apt -y upgrade && sudo apt -y autoremove
+sudo apt -y install nano tmux htop
 
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install protobuf==3.3.0
 
 sudo apt-get install python3-pip libjpeg-dev libopenblas-dev libopenmpi-dev libomp-dev
 sudo -H pip3 install future
-sudo pip3 install -U --user wheel mock pillow
+sudo -H pip3 install -U --user wheel mock pillow
 sudo -H pip3 install testresources setuptools==58.3.0 Cython gdown
 gdown "https://drive.google.com/uc?id=1TqC6_2cwqiYacjoLhLgrZoap6-sVL2sd"
 sudo -H pip3 install torch-1.10.0a0+git36449ea-cp36-cp36m-linux_aarch64.whl
